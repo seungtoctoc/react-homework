781b85bde441f11baf9d1e18ac45acdd55f0c6cb
seungtoctoc <dltmxor31@naver.com>
2024-02-19 21:45:40 +0900

    feat: post reply

--- wadiz-express/routes/index.js
+++ wadiz-express/routes/index.js
@@ -18,7 +18,36 @@ router.post('/:campaignId/comment/:commentId', async function(req, res, next) {
   try {
     const campaignId = req.params.campaignId;
     const commentId = req.params.commentId;
+    const { body, nickName } = req.body;
+    
+    // 댓글의 commentReplys 배열 크기로 depth 구하기
+    const comment = await Comment.findOne({_id: commentId});
+    const depth = comment.commentReplys.length + 1;
 
+    // 대댓글 저장
+    const savedComment = await Comment.create({
+      body: body,
+      Campaign: campaignId,
+      commentType: null,
+      nickName: nickName,
+      whenCreated : new Date(),
+      depth: depth,
+      commentReplys: []
+    })
+
+    // 댓글의 commentReplys 업데이트
+    await Comment.findByIdAndUpdate(commentId,
+      { $push: { commentReplys: savedComment._id }},
+      { new: true }
+    );
+
+    // 대댓글의 commentReplys 업데이트
+    await Comment.findByIdAndUpdate(savedComment._id,
+      { $push: { commentReplys: savedComment._id }},
+      { new: true }
+    );
+
+    res.send(savedComment);
   }
   catch(err) {
     res.send('err: ' + err);
@@ -28,7 +57,7 @@ router.post('/:campaignId/comment/:commentId', async function(req, res, next) {
 router.post('/:campaignId/comment', async function(req, res, next) {
   try {
     const campaignId = req.params.campaignId;
-    const { body, nickName, depth } = req.body;
+    const { body, nickName } = req.body;
 
     const savedComment = await Comment.create({
       body: body,
@@ -36,7 +65,7 @@ router.post('/:campaignId/comment', async function(req, res, next) {
       commentType: null,
       nickName: nickName,
       whenCreated : new Date(),
-      depth: depth,
+      depth: 0,
       commentReplys: []
     })
 
@@ -51,7 +80,7 @@ router.get('/:campaignId', async function(req, res, next) {
   try {
     const campaignId = req.params.campaignId;
 
-    const campaign = await Campaign.find({_id: campaignId})
+    const campaign = await Campaign.findOne({_id: campaignId})
     const comments = await Comment.find({Campaign: campaignId})
 
     res.send({

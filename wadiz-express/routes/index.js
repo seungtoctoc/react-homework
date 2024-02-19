f6e2c4605a1deaa580898a1aa295796ee63323b5
seungtoctoc <dltmxor31@naver.com>
2024-02-19 20:53:54 +0900

    feat: post functions

--- wadiz-express/routes/index.js
+++ wadiz-express/routes/index.js
@@ -4,17 +4,65 @@ var router = express.Router();
 const Campaign = require('../models/campaign-schema');
 const Comment = require('../models/comment-schema');
 
-router.get('/campaign', function(req, res, next) {
-  Campaign.find({})
-    .then(result => {
-      res.send(result);
-    })
-    .catch(err => {
-      res.send(err);
-    })
-
+router.get('/campaign', async function(req, res, next) {
+  try {
+    const campaigns = Campaign.find({})
+    res.send(campaigns);
+  }
+  catch(err) {
+    res.send('err: ' + err);
+  }
 });
 
+router.post('/:campaignId/comment/:commentId', async function(req, res, next) {
+  try {
+    const campaignId = req.params.campaignId;
+    const commentId = req.params.commentId;
 
+  }
+  catch(err) {
+    res.send('err: ' + err);
+  }
+});
+
+router.post('/:campaignId/comment', async function(req, res, next) {
+  try {
+    const campaignId = req.params.campaignId;
+    const { body, nickName, depth } = req.body;
+
+    const savedComment = await Comment.create({
+      body: body,
+      Campaign: campaignId,
+      commentType: null,
+      nickName: nickName,
+      whenCreated : new Date(),
+      depth: depth,
+      commentReplys: []
+    })
+
+    res.send(savedComment);
+  }
+  catch(err) {
+    res.send('err: ' + err);
+  }
+});
+
+router.get('/:campaignId', async function(req, res, next) {
+  try {
+    const campaignId = req.params.campaignId;
+
+    const campaign = await Campaign.find({_id: campaignId})
+    const comments = await Comment.find({Campaign: campaignId})
+
+    res.send({
+      campaign: campaign,
+      comments: comments
+    })
+  }
+  catch(err) {
+    res.send('err: ' + err);
+  }
+
+})
 
 module.exports = router;

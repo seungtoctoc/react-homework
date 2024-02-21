25eda1c702365ad09ab7ce4d4606cf970df602eb
seungtoctoc <102455571+seungtoctoc@users.noreply.github.com>
2024-02-21 22:14:10 +0900

    feat: set text in card

--- wadiz-react/src/components/Campaign.js
+++ wadiz-react/src/components/Campaign.js
@@ -11,21 +11,15 @@ export default function Campaign(props) {
       <Card style={{ width: "18rem" }}>
         <Card.Img variant="top" src={campaign.photoUrl} />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
+          <Card.Text>{campaign.title}</Card.Text>
         </Card.Body>
         <ListGroup className="list-group-flush">
-          <ListGroup.Item>Cras justo odio</ListGroup.Item>
-          <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
-          <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+          <ListGroup.Item>{campaign.achievementRate}% 달성률</ListGroup.Item>
+          <ListGroup.Item>{campaign.nickName}</ListGroup.Item>
+          <ListGroup.Item>
+            <small>{campaign.categoryName}</small>
+          </ListGroup.Item>
         </ListGroup>
-        <Card.Body>
-          <Card.Link href="#">Card Link</Card.Link>
-          <Card.Link href="#">Another Link</Card.Link>
-        </Card.Body>
       </Card>
     </div>
   );
